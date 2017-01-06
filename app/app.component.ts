5d302c86c964bb9493850391cfcaa97e99f34f35
Tyler Cooper <twcstuff@hotmail.com>
2017-01-06 13:00:48 -0700

    First component commit

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,16 +1,15 @@
 import { Component } from 'angular2/core';
 
 @Component({
-    selector: 'events-app', 
-    template: '
+    selector: 'events-app',
+    template: `
     <div><h1>{{pageTitle}}</h1>
          <div> Super Simple Component </div>
     </div>
-    '
-  
+    `
+
 })
 
 export class AppComponent {
    pageTitle: string = 'Local Events App';
-   
 }
